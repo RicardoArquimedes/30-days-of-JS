4a9e046f3c7dcb62fabf3d16220394ecd9329166
RicardoArquimedes <richardcqt28@gmail.com>
2023-03-06 12:29:28 -0500

    :sparkles: Day 1: create a function to return  the type

---
+++ 01-type.js
@@ -0,0 +1,10 @@
+
+// create a function that return the type of the value
+
+function solution(value) {
+    return typeof (value)
+}
+
+console.log(solution(1))
+console.log(solution("Richie"))
+console.log(solution(false))
