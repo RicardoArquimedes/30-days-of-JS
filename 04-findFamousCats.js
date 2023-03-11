8ddacad2e80bd47462897f244056220d170f748d
RicardoArquimedes <richardcqt28@gmail.com>
2023-03-10 19:31:54 -0500

    :sparkles: adding findLargestPalindrome function for day 05

--- 04-findFamousCats.js
+++ 04-findFamousCats.js
@@ -1,11 +1,10 @@
-export function findFamousCats(cats) {
-    let obj = {};
-  
-    cats.forEach(element => {
-      return obj[element.name] = element.followers.reduce((a, b) => a + b)
-    });
-  
-    let max = Math.max(...Object.values(obj));
-    return (Object.keys(obj).filter(key => obj[key] === max));
-  };
-  
+function findFamousCats(cats) {
+  let obj = {};
+
+  cats.forEach(element => {
+    return obj[element.name] = element.followers.reduce((a, b) => a + b)
+  });
+
+  let max = Math.max(...Object.values(obj));
+  return (Object.keys(obj).filter(key => obj[key] === max));
+};
